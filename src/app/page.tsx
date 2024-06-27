69043180bfdc63eb9c2ffe7ad9499f46a0573277
Kolapo Wariz <kolapowariz@gmail.com>
2024-06-27 12:57:10 +0100

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p>My portfolio</p>
+      <p className="bg-green-800">My portfolio</p>
     </div>
   );
 }
