347cf980239b97cc3745bc3313d6ee3a40ba67da
Kolapo Wariz <kolapowariz@gmail.com>
2024-08-30 15:29:37 +0100

    |

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
 'use client';
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { motion } from 'framer-motion'
-import { ProjectType } from "@/app/types";
-import DashboardSkeleton from "./ui/skeletons";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   const [click, setClick] = useState(false);
-  const [projects, setProjects] = useState<ProjectType[]>([]);
-  useEffect(() => {
-    fetch('/data/portfolio.json')
-      .then(res => res.json())
-      .then(data => setProjects(data))
-      .catch((error) => {
-        console.error('Error fetching the portfolio data:', error);
-        setProjects([]);
-      });
-  }, []);
-
-
+  
   const toggle = () => {
     setClick(prev => !prev)
   }
@@ -90,29 +78,7 @@ export default function Home() {
           </section>
         </section>
 
-        <section id="projects">
-          <h2 className='text-center text-4xl mt-10'>Projects</h2>
-          <div className='xl:grid lg:grid-cols-2 lg:gap-4'>
-            <>
-              {projects.length === 0 && <DashboardSkeleton />}
-            </>
-            {projects.map((data) => {
-              return (
-                <div key={data.id} className='sm:w-full flex flex-col sm:flex-row justify-center items-center my-7 mx-auto'>
-                  <div className='w-full h-auto rounded-t-xl sm:rounded-none sm:w-96 sm:h-52 bg-gray-200 text-black sm:rounded-s-2xl p-4'>
-                    <h1 className="text-center text-2xl mb-2">{data.title}</h1>
-                    <p className='mb-4'>{data.littleDesc}</p>
-                    <Link href={`/${data.title}`} className='text-blue-500 border-slate-800 border rounded-lg p-2'>View Project</Link>
-
-                  </div>
-                  <div className='w-full h-auto rounded-b-xl sm:rounded-none sm:w-96 sm:h-52 bg-gray-200 sm:rounded-e-2xl p-4'>
-                    <Image src={data.image} alt={data.imageAlt} width={400} height={400} className='mx-auto w-full h-auto rounded-lg ' />
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </section>
+        <Projects />
         <section id='contact' className='mt-10 w-auto sm:w-96 mx-auto'>
           <h2 className='text-center text-4xl'>Contact</h2>
           <form action="" className="mt-7 text-black">
---
+++ src/components/Projects.tsx
@@ -0,0 +1,45 @@
+'use client';
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { motion } from 'framer-motion'
+import { ProjectType } from "@/app/types";
+import DashboardSkeleton from "../app/ui/skeletons";
+
+export default function Projects() {
+  const [projects, setProjects] = useState<ProjectType[]>([]);
+  useEffect(() => {
+    fetch('/data/portfolio.json')
+      .then(res => res.json())
+      .then(data => setProjects(data))
+      .catch((error) => {
+        console.error('Error fetching the portfolio data:', error);
+        setProjects([]);
+      });
+  }, []);
+  return (
+    <section id="projects">
+          <h2 className='text-center text-4xl mt-10'>Projects</h2>
+          <div className='xl:grid lg:grid-cols-2 lg:gap-4'>
+            <>
+              {projects.length === 0 && <DashboardSkeleton />}
+            </>
+            {projects.map((data) => {
+              return (
+                <div key={data.id} className='sm:w-full flex flex-col sm:flex-row justify-center items-center my-7 mx-auto'>
+                  <div className='w-full h-auto rounded-t-xl sm:rounded-none sm:w-96 sm:h-52 bg-gray-200 text-black sm:rounded-s-2xl p-4'>
+                    <h1 className="text-center text-2xl mb-2">{data.title}</h1>
+                    <p className='mb-4'>{data.littleDesc}</p>
+                    <Link href={`/${data.title}`} className='text-blue-500 border-slate-800 border rounded-lg p-2'>View Project</Link>
+
+                  </div>
+                  <div className='w-full h-auto rounded-b-xl sm:rounded-none sm:w-96 sm:h-52 bg-gray-200 sm:rounded-e-2xl p-4'>
+                    <Image src={data.image} alt={data.imageAlt} width={400} height={400} className='mx-auto w-full h-auto rounded-lg ' />
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </section>
+  )
+}
