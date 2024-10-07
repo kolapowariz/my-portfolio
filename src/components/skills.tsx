cdff04411a045ef1b2566ce41bb63caa9b36091c
Kolapo Wariz <kolapowariz@gmail.com>
2024-10-07 13:49:58 +0100

    updated skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -4,7 +4,7 @@ export default function Skills() {
     <section id="skills" className='mt-4'>
       {/* <h2 className='text-center text-4xl'>Skills</h2> */}
       <h3 className="text-3xl underline underline-offset-8">Technologies</h3>
-      <p className="text-xl my-5">HTML, CSS, JavaScript, TypeScript, ReactJS, VueJS, NextJS,  MySQL, Supabase, PostgreSQL.</p>
+      <p className="text-xl my-5">HTML, CSS, JavaScript, TypeScript, ReactJS, VueJS, NextJS, SQL, Supabase, PostgreSQL.</p>
       <h3 className="text-3xl underline underline-offset-8">Test</h3>
       <p className="text-xl my-5">Jest, Vitest, Cypress.</p>
       <h3 className="text-3xl underline underline-offset-8">UI Libraries</h3>
