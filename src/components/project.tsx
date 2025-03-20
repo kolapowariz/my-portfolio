d3a22eb7ab790f1e18be48322ffce7ac2341bacd
Kolapo Wariz <kolapowariz@gmail.com>
2025-03-20 14:04:48 +0100

    Work experience added

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const projects = [
   {
@@ -96,42 +96,65 @@ const projects = [
   },
 ];
 
-
 export default function Projects() {
   return (
     <section id="projects" className="pt-10">
-      <h2 className='text-center text-4xl mt-10'>Projects</h2>
-      <div className='xl:grid lg:mt-6 xl:grid-cols-2 xl:gap-8'>
+      <h2 className="text-center text-4xl mt-10">Projects</h2>
+      <div className="xl:grid lg:mt-6 xl:grid-cols-2 xl:gap-8">
         {projects.map((data) => {
           return (
-            <section key={data.id} className="bg-[#2a455e] xl:w-[100%] lg:w-[80%] sm:w-[90%] rounded-xl mx-auto">
-
-              <div className='bg-[#2a455e] sm:w-full rounded-lg flex flex-col sm:flex-row justify-center items-center my-7 mx-auto mb-0'>
-                <div className='bg-[#2a455e] w-full h-auto rounded-t-xl sm:rounded-none sm:w-96 sm:h-48 sm:rounded-s-2xl p-4'>
+            <section
+              key={data.id}
+              className="bg-[#2a455e] xl:w-[100%] lg:w-[80%] sm:w-[90%] rounded-xl mx-auto"
+            >
+              <div className="bg-[#2a455e] sm:w-full rounded-lg flex flex-col sm:flex-row justify-center items-center my-7 mx-auto mb-0">
+                <div className="bg-[#2a455e] w-full h-auto rounded-t-xl sm:rounded-none sm:w-96 sm:h-48 sm:rounded-s-2xl p-4">
                   <h1 className="text-center text-2xl mb-2">{data.title}</h1>
-                  <p className='mb-4'>{data.littleDesc}</p>
+                  <p className="mb-4">{data.littleDesc}</p>
                   <div className="flex gap-2 justify-center">
-                    <a href={data.liveUrl} target="_blank" className='text-blue-500 border-white border rounded-lg p-2'>Live URL</a>
-                    <a href={data.githubUrl} target="_blank" className='text-blue-500 border-white border rounded-lg p-2'>Repository</a>
+                    <a
+                      href={data.liveUrl}
+                      target="_blank"
+                      className="text-blue-500 border-white border rounded-lg p-2"
+                    >
+                      Live URL
+                    </a>
+                    <a
+                      href={data.githubUrl}
+                      target="_blank"
+                      className="text-blue-500 border-white border rounded-lg p-2"
+                    >
+                      Repository
+                    </a>
                   </div>
-
                 </div>
-                <div className='w-full h-auto rounded-b-xl sm:rounded-none sm:w-96 sm:h-48 bg-[#2a455e] sm:rounded-e-2xl p-4'>
-                  <Image src={data.image} alt={data.imageAlt} width={400} height={400} className='mx-auto w-full h-auto rounded-lg ' />
+                <div className="w-full h-auto rounded-b-xl sm:rounded-none sm:w-96 sm:h-48 bg-[#2a455e] sm:rounded-e-2xl p-4">
+                  <Image
+                    src={data.image}
+                    alt={data.imageAlt}
+                    width={400}
+                    height={400}
+                    className="mx-auto w-full h-auto rounded-lg "
+                  />
                 </div>
               </div>
               <section className="bg-[#2a455e] p-2 rounded-xl">
                 <p className="text-center mb-2">Technologies</p>
                 <ul className="flex flex-wrap gap-2 justify-center">
                   {data.technologies.map((tech) => (
-                    <li key={tech} className='bg-[#4c6d8b] rounded-lg p-2 text-center'>{tech}</li>
+                    <li
+                      key={tech}
+                      className="bg-[#4c6d8b] rounded-lg p-2 text-center"
+                    >
+                      {tech}
+                    </li>
                   ))}
                 </ul>
               </section>
             </section>
-          )
+          );
         })}
       </div>
     </section>
-  )
+  );
 }
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,24 +1,58 @@
 export default function Skills() {
-
   return (
-    <section id="skills" className='mt-4'>
+    <section id="skills" className="mt-4">
       <h3 className="text-3xl underline underline-offset-8">Education</h3>
-      <p className="text-xl my-5">I hold a Diploma in Frontend Engineering from AltSchool Africa, where I developed expertise in web development using HTML, CSS, TypeScript, and JavaScript, along with libraries like React.js and frameworks like Vue.js and Next.js. My previous National Diploma in Computer Science from the Federal School of Statistics provided me with a solid foundation in programming, database management, and statistical analysis, supporting my growth in the tech sector
+      <p className="text-xl my-5">
+        I hold a Diploma in Frontend Engineering from AltSchool Africa, where I
+        developed expertise in web development using HTML, CSS, TypeScript, and
+        JavaScript, along with libraries like React.js and frameworks like
+        Vue.js and Next.js. My previous National Diploma in Computer Science
+        from the Federal School of Statistics provided me with a solid
+        foundation in programming, database management, and statistical
+        analysis, supporting my growth in the tech sector
       </p>
-      <p className="text-xl my-5">I am currently pursuing a Bachelor of Science in Computer Science at the University of Ibadan, focusing on software development, data structures, algorithms, and artificial intelligence. Hands-on projects have enhanced my problem-solving skills and knowledge of programming languages and software design.
+      <p className="text-xl my-5">
+        I am currently pursuing a Bachelor of Science in Computer Science at the
+        University of Ibadan, focusing on software development, data structures,
+        algorithms, and artificial intelligence. Hands-on projects have enhanced
+        my problem-solving skills and knowledge of programming languages and
+        software design.
       </p>
-      {/* <h3 className="text-3xl underline underline-offset-8">Work Experience</h3>
-      <p className="text-xl my-5">...</p> */}
+      <h3 className="text-3xl underline underline-offset-8">Work Experience</h3>
+      <p className="text-xl my-5">
+        Frontend Developer Intern, Enextgen Wireless Limited, Nigeria | August
+        2024 – October 2024
+      </p>
+      <p className="text-xl my-5">
+        During my internship at Enextgen Wireless Limited, I optimized and built
+        responsive user interfaces using React, Shadcn UI, and Tailwind CSS,
+        leading to a 20% improvement in performance and reduced load times. I
+        worked closely with an Agile development team to create user-friendly
+        internet speed test web applications, ensuring a seamless experience
+        across different devices. My role involved integrating PostgreSQL for
+        efficient data management, enhancing database performance and data
+        retrieval speed. Additionally, I collaborated with backend developers to
+        implement APIs, debug issues, and enhance the application&apos;s
+        functionality, improving overall system reliability.
+      </p>
       <h3 className="text-3xl underline underline-offset-8">Technologies</h3>
-      <p className="text-xl my-5">HTML, CSS, JavaScript, TypeScript, ReactJS, VueJS, NextJS, SQL, Supabase, PostgreSQL.</p>
+      <p className="text-xl my-5">
+        HTML, CSS, JavaScript, TypeScript, ReactJS, VueJS, NextJS, Supabase,
+        PostgreSQL, MongoDB, MySQL.
+      </p>
       <h3 className="text-3xl underline underline-offset-8">Test</h3>
       <p className="text-xl my-5">Jest, Vitest.</p>
       <h3 className="text-3xl underline underline-offset-8">UI Libraries</h3>
       <p className="text-xl my-5">Tailwind CSS, Framer Motion, Shadcn UI.</p>
       <h3 className="text-3xl underline underline-offset-8">Tools</h3>
-      <p className="text-xl my-5">Git, GitHub, Vercel, Netlify, Figma, VSCode, Vite, ESLint, Prettier, Webpack, Stackblitz, CodeSandBox.</p>
+      <p className="text-xl my-5">
+        Git, GitHub, Vercel, Netlify, Figma, VSCode, Vite, ESLint, Prettier,
+        Webpack, Stackblitz, CodeSandBox.
+      </p>
       <h3 className="text-3xl underline underline-offset-8">Soft Skills</h3>
-      <p className="text-xl my-5">Problem Solving, Communicaton, Teamwork, Time Management.</p>
+      <p className="text-xl my-5">
+        Problem Solving, Communicaton, Teamwork, Time Management.
+      </p>
     </section>
-  )
+  );
 }
